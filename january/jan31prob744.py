d1fad63dd5c702c1aa5c8bd94cbc7107dd5dae75
nishan <bhattarainisham@gmail.com>
2026-01-31 09:04:31 +0545

    Add solution for problem 744: Next Greatest Letter

---
+++ january/jan31prob744.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        l, r = 0, len(letters) - 1
+        ans = letters[0]  # wrap-around default
+
+        while l <= r:
+            mid = (l + r) // 2
+            if letters[mid] > target:
+                ans = letters[mid]
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return ans
